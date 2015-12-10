1ca2a1403702aadbecf226cc35e197315034c30d
karlobrien <karlobrien@gmail.com>
2015-12-10 06:14:22 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Advent of Code on Node");
+
+import {createServer} from 'http';
+
+const headers = Object.freeze({ 'Content-Type': 'text/plain'});
+
+const server = createServer((req, res) => {
+    res.writeHead(200, headers);
+
+    let message = `ES6 - AAAAAgain!\n`;
+    res.end(message);
+});
+
+let test = [1,2,3].map(x => x * 2);
+console.log(test);
+
+server.listen(8888);
